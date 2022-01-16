fed9dd1bc4ae37f62e780bf71bea155ef1778f50
Sudarshan Kumar <venkaksub@gmail.com>
2022-01-15 20:24:35 -0500

    gui file from pycharm

---
+++ ymcaGUI.py
@@ -0,0 +1,34 @@
+import cv2
+
+print("run")
+img = cv2.imread("/Users/sudarshankumar/PycharmProjects/ymca/venv/external-content.duckduckgo.com.jpg")
+
+text = "Welcome to The YMCA Detector /nby The Village People!"
+
+y_start = 100
+y_inc = 550
+x_start = 10
+x_inc = 225
+for i, line in enumerate(text.split("/n")):
+    y = y_start + i * y_inc
+    x = x_start + i * x_inc
+    cv2.putText(img, line, (x, y), 2, 2.5, (0, 0, 255), 1, cv2.FONT_HERSHEY_DUPLEX)
+
+cv2.imshow("Welcome Page", img)
+
+
+def doYMCA(event, x, y, flags, params):
+    print(event)
+    if event == cv2.EVENT_LBUTTONDOWN:
+        cv2.destroyAllWindows()
+        imgCopy = img.copy()
+        cv2.imshow("Y", imgCopy)
+        #insert script name to execute
+        #exec(open("").read())
+
+
+# if left click down
+cv2.setMouseCallback("YMCA Detector", doYMCA)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
