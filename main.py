a576bb435d8ae9edb917ac3775e03fe1ae6549bd
Zan_88 <7388931+zan-88@users.noreply.github.com>
2022-01-15 19:34:15 -0500

    rest of project

---
+++ main.py
@@ -0,0 +1,19 @@
+from playsound import playsound
+import pygame
+import os
+from pychromecast import controllers
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+
+def search_the_village():
+   playsound('ymca.mp3')
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    search_the_village()
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
